d691405bcd83b182a531616d557fae6adcb7617e
Ayush <ayushgaming145@gmail.com>
2026-04-08 23:32:20 +0530

    Database finalise
    Inserted multiple hospital records into the database.

--- database.sql
+++ database.sql
@@ -6,3 +6,21 @@ CREATE TABLE hospitals (
     latitude DOUBLE,
     longitude DOUBLE
 );
+
+INSERT INTO hospitals (name, latitude, longitude) VALUES
+('BM Shah Hospital', 21.2097, 81.3601),
+('Hi-Tech Hospital Bhilai', 21.216022, 81.322815),
+('Sparsh Hospital', 21.20559, 81.35913),
+('Vayam Hospital', 21.20429, 81.35913),
+('SBS Hospital', 21.2062, 81.3644),
+('Mittal Hospital', 21.2164, 81.3226),
+('Sector 9 Hospital', 21.1888, 81.3151),
+
+('Aarogya Hospital Durg', 21.2052, 81.3094),
+('Jawahar Lal Nehru Hospital', 21.1558, 81.3564),
+('Chandulal', 21.2003, 81.3226),
+('Gangotri Hospital', 21.1892, 81.2821),
+('District Hospital Durg', 21.1900, 81.2840),
+('Maa Chandika Hospital', 21.1875, 81.2790),
+('Vardhman Hospital', 21.1915, 81.2860),
+('RR Hospital', 21.1925, 81.2875);
