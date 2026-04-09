f3ecfeb4086498d3f425d42345eb010507e83692
Ayush <ayushgaming145@gmail.com>
2026-04-10 00:04:46 +0530

    Extract hospital name and coordinates from input

--- map.php
+++ map.php
@@ -3,3 +3,10 @@ $current = $_GET['current_location'];
 $hospital_data = $_GET['hospital_location'];
 
 $parts = explode("|", $hospital_data);
+
+$hospital_name = $parts[0];
+$coords = explode(",", $parts[1]);
+
+$destLat = $coords[0];
+$destLon = $coords[1];
+?>
