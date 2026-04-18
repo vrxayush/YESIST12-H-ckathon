9496bdc3489a015482cea4eaea5a57a64227a0a8
Ayush <ayushgaming145@gmail.com>
2026-04-18 23:50:33 +0530

    Add title and Leaflet resources to normal_view.php

--- normal_view.php
+++ normal_view.php
@@ -5,3 +5,10 @@ $back = isset($_GET['back']) ? $_GET['back'] : 'map.php';
 <!DOCTYPE html>
 <html>
 <head>
+
+    <title>Normal User View</title>
+
+    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css"/>
+    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+
+    <style>
