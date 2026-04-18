bb5f59a26414f75d994c25b01e2dae6f02f93bcc
Ayush <ayushgaming145@gmail.com>
2026-04-18 23:50:51 +0530

    Style body with custom font and background color
    Add custom styles for body in normal_view.php

--- normal_view.php
+++ normal_view.php
@@ -12,3 +12,10 @@ $back = isset($_GET['back']) ? $_GET['back'] : 'map.php';
     <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
 
     <style>
+
+        body {
+            margin: 0;
+            font-family: 'Segoe UI';
+            background: #0f172a;
+            color: white;
+        }
